788f109fc632ee2b04d6083b30ffe1c5970af163
Net <hoangminhtuan1399@gmail.com>
2024-09-03 16:38:11 +0700

    tuan - import template layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { SharedModule } from "./shared/shared.module";
+import { HomeModule } from "./home/home.module";
 
 @NgModule({
   declarations: [
@@ -10,8 +13,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    HomeModule,
+    SharedModule
   ],
+  exports: [],
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/home/home.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
+import { CommonModule } from "@angular/common";
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+
+export class HomeModule {
+}
