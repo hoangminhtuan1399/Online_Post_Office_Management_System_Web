ba4f9141ce5e1192fe414306d81bac1f06a3bb07
Net <hoangminhtuan1399@gmail.com>
2024-09-16 22:31:08 +0700

    tuan - admin restriction on dashboard
    implement admin restriction

--- src/app/admin/components/sidebar/sidebar.component.ts
+++ src/app/admin/components/sidebar/sidebar.component.ts
@@ -1,25 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from "../../auth.service";
 
 
 @Component({
-    selector: 'app-sidebar',
-    templateUrl: './sidebar.component.html',
-    styleUrl: './sidebar.component.css'
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
 })
 
-export class SidebarComponent {
-    isCollapsed = true;
+export class SidebarComponent implements OnInit {
+  isCollapsed = true;
+  userRole: string | null = null;
 
-    constructor(protected router: Router, private authService: AuthService) {}
+  constructor(protected router: Router, private authService: AuthService) {
+  }
 
-    logout() {
-        this.authService.signOut();
-        this.router.navigate(['/admin/login']); // Redirect to the login page
-    }
+  ngOnInit(): void {
+    this.userRole = localStorage.getItem('userRole');
+  }
 
-    isActive(url: string): boolean {
-        return this.router.url === url;
-    }
+  logout() {
+    localStorage.removeItem('userRole');
+    this.authService.signOut();
+    this.router.navigate(['/admin/login']); // Redirect to the login page
+  }
+
+  isActive(url: string): boolean {
+    return this.router.url === url;
+  }
+
+  hasAdminAccess(): boolean {
+    return this.userRole === 'admin';
+  }
 }
