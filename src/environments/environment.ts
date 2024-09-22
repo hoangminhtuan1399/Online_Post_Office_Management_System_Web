65ae5b5a9452457b53ea8d099d04aa1951be371f
bao long <pikalong0311@gmail.com>
2024-09-22 15:13:16 +0700

    Long - update connect to BE

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiBaseUrl: 'https://localhost:7029'
+  apiBaseUrl: 'https://onlinepostofficemanagementapi-a7csemagdjdecbbu.southeastasia-01.azurewebsites.net'
 };
