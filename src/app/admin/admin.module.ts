586368447f62d9d5a56eb314b47cf93a6b293bb3
vupot123 <vupot123@gmail.com>
2024-09-22 20:28:54 +0700

    commit message

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -8,7 +8,6 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { LoginComponent } from './components/login/login.component';
 import { PaymentsComponent } from './components/payments/payments.component';
 import { BranchesComponent } from './components/branches/branches.component';
-import { ServicesComponent } from './components/services/services.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { AdminLayoutComponent } from './components/admin-layout/admin-layout.component';
@@ -18,13 +17,16 @@ import { OfficeListComponent } from './components/offices/office-list/office-lis
 import { PackageComponent } from './components/package/package.component';
 import { EmployeeEditComponent } from './components/employees/employee-edit/employee-edit.component';
 import { EmployeeDetailComponent } from './components/employees/employee-detail/employee-detail.component';
+import { ServiceEditComponent } from './components/services/service-edit/service-edit.component';
+import { ServiceDetailComponent } from './components/services/service-detail/service-detail.component';
+import { ServiceListComponent } from './components/services/service-list/service-list.component';
+import { ServiceCreateComponent } from './components/services/service-create/service-create.component';
 @NgModule({
   declarations: [
     DashboardComponent,
     LoginComponent,
     PaymentsComponent,
     BranchesComponent,
-    ServicesComponent,
     ProfileComponent,
     SidebarComponent,
     AdminLayoutComponent,
@@ -33,7 +35,11 @@ import { EmployeeDetailComponent } from './components/employees/employee-detail/
     OfficeListComponent,
     PackageComponent,
     EmployeeEditComponent,
-    EmployeeDetailComponent
+    EmployeeDetailComponent,
+    ServiceEditComponent,
+    ServiceDetailComponent,
+    ServiceListComponent,
+    ServiceCreateComponent
   ],
   imports: [
     CommonModule,
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,13 +6,18 @@ import { AuthGuard } from "./admin/auth.guard";
 import { LoginComponent } from "./admin/components/login/login.component";
 import { PaymentsComponent } from "./admin/components/payments/payments.component";
 import { BranchesComponent } from "./admin/components/branches/branches.component";
-import { ServicesComponent } from "./admin/components/services/services.component";
 import { ProfileComponent } from "./admin/components/profile/profile.component";
 import { AdminLayoutComponent } from "./admin/components/admin-layout/admin-layout.component";
 import { EmployeeListComponent } from './admin/components/employees/employee-list/employee-list.component';
 import { EmployeeCreateComponent } from './admin/components/employees/employee-create/employee-create.component';
 import { EmployeeEditComponent } from './admin/components/employees/employee-edit/employee-edit.component';  // Import EmployeeEditComponent
 import { EmployeeDetailComponent } from './admin/components/employees/employee-detail/employee-detail.component'; // Import EmployeeDetailComponent
+
+// Import thêm các component của Service
+import { ServiceListComponent } from './admin/components/services/service-list/service-list.component';
+import { ServiceCreateComponent } from './admin/components/services/service-create/service-create.component';
+import { ServiceEditComponent } from './admin/components/services/service-edit/service-edit.component';
+import { ServiceDetailComponent } from './admin/components/services/service-detail/service-detail.component';
 
 const routes: Routes = [
   { path: 'admin/login', component: LoginComponent },
@@ -28,7 +33,10 @@ const routes: Routes = [
       { path: 'employees/create', component: EmployeeCreateComponent },
       { path: 'employees/edit/:id', component: EmployeeEditComponent },  
       { path: 'employees/detail/:id', component: EmployeeDetailComponent }, 
-      { path: 'services', component: ServicesComponent },
+      { path: 'services/list', component: ServiceListComponent },
+      { path: 'services/create', component: ServiceCreateComponent },
+      { path: 'services/edit/:id', component: ServiceEditComponent },
+      { path: 'services/detail/:id', component: ServiceDetailComponent },
       { path: 'profile', component: ProfileComponent },
       { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
       { path: '**', redirectTo: 'dashboard' }
