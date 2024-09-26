2b3e77f41f2dbf70ff35a6ab1777e6fa7d85468d
vupot123 <vupot123@gmail.com>
2024-09-26 23:46:57 +0700

    commit message

--- src/app/admin/components/services/service-list/service-list.component.ts
+++ src/app/admin/components/services/service-list/service-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { ServiceService } from '../../services/service.service';
 import { Service } from '../../../models/service.model';
 import { Router } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-service-list',
@@ -13,10 +13,12 @@ export class ServiceListComponent implements OnInit {
   services: Service[] = [];
   isLoading = false;
   selectedServiceId: string | null = null;
+  modalRef: NgbModalRef | null = null; // Quản lý tham chiếu modal
 
   @ViewChild('createServiceModal') createServiceModal: any;
   @ViewChild('editServiceModal') editServiceModal: any;
   @ViewChild('detailServiceModal') detailServiceModal: any;
+
   constructor(
     private serviceService: ServiceService,
     private router: Router,
@@ -34,7 +36,7 @@ export class ServiceListComponent implements OnInit {
         this.services = data;
       },
       (error) => {
-        console.error('Error fetching Service data', error);
+        console.error('Error fetching service data', error);
       },
       () => {
         this.isLoading = false;
@@ -48,11 +50,25 @@ export class ServiceListComponent implements OnInit {
 
   openDetailServiceModal(serviceId: string): void {
     this.selectedServiceId = serviceId;  
-    this.modalService.open(this.detailServiceModal, { centered: true });  
+    this.modalRef = this.modalService.open(this.detailServiceModal, { centered: true });  
   }
-  
+
   openEditServiceModal(serviceId: string): void {
     this.selectedServiceId = serviceId; 
-    this.modalService.open(this.editServiceModal, { centered: true });  
+    this.modalRef = this.modalService.open(this.editServiceModal, { centered: true });  
   }
-}
+
+  closeDetailServiceModal(): void {
+    if (this.modalRef) {
+      this.modalRef.close();
+      this.modalRef = null;
+  
+      
+      window.location.reload();
+    }
+  }
+  
+  }
+  
+
+
